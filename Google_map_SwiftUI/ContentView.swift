45d00973b88a83ea76562c4e5dc17a73c261448e
Alexandra Makey <aliaksandra.makey@gmail.com>
2023-11-15 14:04:15 +0700

    Initial Commit

---
+++ Google_map_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Google_map_SwiftUI
+//
+//  Created by Александра Макей on 15.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Google_map_SwiftUI/Google_map_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_map_SwiftUIApp.swift
+//  Google_map_SwiftUI
+//
+//  Created by Александра Макей on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Google_map_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
